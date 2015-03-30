b50028f8431dffcf7428f2414281cb78d8b32b84
Shannon <sdeminick@gmail.com>
2015-03-31 10:46:13 +1100

    Commits code with build files

---
+++ build/Build-Release.ps1
@@ -0,0 +1,88 @@
+param (
+	[Parameter(Mandatory=$true)]
+	[ValidatePattern("^\d\.\d\.(?:\d\.\d$|\d$)")]
+	[string]
+	$ReleaseVersionNumber,
+	[Parameter(Mandatory=$true)]
+	[string]
+	[AllowEmptyString()]
+	$PreReleaseName
+)
+
+$PSScriptFilePath = (Get-Item $MyInvocation.MyCommand.Path);
+$RepoRoot = (get-item $PSScriptFilePath).Directory.Parent.FullName;
+$SolutionRoot = Join-Path -Path $RepoRoot "src";
+
+#trace
+"Solution Root: $SolutionRoot"
+
+$MSBuild = "$Env:SYSTEMROOT\Microsoft.NET\Framework\v4.0.30319\msbuild.exe";
+
+# Make sure we don't have a release folder for this version already
+$BuildFolder = Join-Path -Path $RepoRoot -ChildPath "build";
+$ReleaseFolder = Join-Path -Path $BuildFolder -ChildPath "Releases\v$ReleaseVersionNumber$PreReleaseName";
+if ((Get-Item $ReleaseFolder -ErrorAction SilentlyContinue) -ne $null)
+{
+	Write-Warning "$ReleaseFolder already exists on your local machine. It will now be deleted."
+	Remove-Item $ReleaseFolder -Recurse
+}
+New-Item $ReleaseFolder -Type directory
+
+#trace
+"Release path: $ReleaseFolder"
+
+# Set the version number in SolutionInfo.cs
+$AssemblyInfoPath = Join-Path -Path $SolutionRoot -ChildPath "UmbracoExamine.PDF\Properties\AssemblyInfo.cs"
+(gc -Path $AssemblyInfoPath) `
+	-replace "(?<=AssemblyFileVersion\(`")[.\d]*(?=`"\))", $ReleaseVersionNumber |
+	sc -Path $AssemblyInfoPath -Encoding UTF8;
+(gc -Path $AssemblyInfoPath) `
+	-replace "(?<=AssemblyInformationalVersion\(`")[.\w-]*(?=`"\))", "$ReleaseVersionNumber$PreReleaseName" |
+	sc -Path $AssemblyInfoPath -Encoding UTF8;
+# Set the copyright
+$Copyright = "Copyright © Umbraco ".(Get-Date).year
+(gc -Path $AssemblyInfoPath) `
+	-replace "(?<=AssemblyCopyright\(`")[.\w-]*(?=`"\))", $Copyright |
+	sc -Path $AssemblyInfoPath -Encoding UTF8;
+
+# Build the solution in release mode
+$SolutionPath = Join-Path -Path $SolutionRoot -ChildPath "UmbracoExamine.PDF.sln";
+
+# clean sln for all deploys
+& $MSBuild "$SolutionPath" /p:Configuration=Release /maxcpucount /t:Clean
+if (-not $?)
+{
+	throw "The MSBuild process returned an error code."
+}
+
+#build
+& $MSBuild "$SolutionPath" /p:Configuration=Release /maxcpucount
+if (-not $?)
+{
+	throw "The MSBuild process returned an error code."
+}
+
+$include = @('UmbracoExamine.PDF.dll','UmbracoExamine.PDF.pdb')
+$CoreBinFolder = Join-Path -Path $SolutionRoot -ChildPath "UmbracoExamine.PDF\bin\Release";
+Copy-Item "$CoreBinFolder\*.*" -Destination $ReleaseFolder -Include $include
+
+# COPY THE TRANSFORMS OVER
+Copy-Item "$BuildFolder\nuget-transforms\ExamineIndex.config.install.xdt" -Destination (New-Item (Join-Path -Path $ReleaseFolder -ChildPath "nuget-transforms") -Type directory);
+Copy-Item "$BuildFolder\nuget-transforms\ExamineIndex.config.uninstall.xdt" -Destination (Join-Path -Path $ReleaseFolder -ChildPath "nuget-transforms");
+Copy-Item "$BuildFolder\nuget-transforms\ExamineSettings.config.install.xdt" -Destination (Join-Path -Path $ReleaseFolder -ChildPath "nuget-transforms");
+Copy-Item "$BuildFolder\nuget-transforms\ExamineSettings.config.uninstall.xdt" -Destination (Join-Path -Path $ReleaseFolder -ChildPath "nuget-transforms");
+
+# COPY THE README OVER
+Copy-Item "$BuildFolder\Readme.txt" -Destination $ReleaseFolder
+
+# COPY OVER THE CORE NUSPEC AND BUILD THE NUGET PACKAGE
+Copy-Item "$BuildFolder\UmbracoCms.UmbracoExamine.PDF.nuspec" -Destination $ReleaseFolder
+$CoreNuSpec = Join-Path -Path $ReleaseFolder -ChildPath "UmbracoCms.UmbracoExamine.PDF.nuspec";
+$NuGet = Join-Path $BuildFolder -ChildPath "NuGet.exe"
+Write-Output "DEBUGGING: " $CoreNuSpec -OutputDirectory $ReleaseFolder -Version $ReleaseVersionNumber$PreReleaseName
+& $NuGet pack $CoreNuSpec -OutputDirectory $ReleaseFolder -Version $ReleaseVersionNumber$PreReleaseName
+
+""
+"Build $ReleaseVersionNumber$PreReleaseName is done!"
+"NuGet packages also created, so if you want to push them just run:"
+"  nuget push $CoreNuSpec"
---
+++ src/UmbracoExamine.PDF/Properties/AssemblyInfo.cs
@@ -0,0 +1,29 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Security;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyCompany("Umbraco")]
+[assembly: AssemblyCopyright("Copyright © Umbraco 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyTitle("UmbracoExamine.PDF")]
+[assembly: AssemblyDescription("Umbraco indexing for PDF files using UmbracoExamine")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyProduct("UmbracoExamine.PDF")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8933a78d-8414-4c72-a74d-76aa7fb0e9ad")]
+
+[assembly: AssemblyVersion("0.8.*")]
+
+[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
