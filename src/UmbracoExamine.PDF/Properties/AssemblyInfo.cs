ca888e5a89b464e2a23b1090a383fdacb05f5a2e
Shannon <sdeminick@gmail.com>
2016-03-17 16:54:53 +0100

    Ensure net40 is added to the nuspec #9

--- src/UmbracoExamine.PDF/Properties/AssemblyInfo.cs
+++ src/UmbracoExamine.PDF/Properties/AssemblyInfo.cs
@@ -25,5 +25,5 @@ using System.Security;
 
 [assembly: AssemblyVersion("0.8.*")]
 
-[assembly: AssemblyFileVersion("1.0.1")]
-[assembly: AssemblyInformationalVersion("1.0.1")]
+[assembly: AssemblyFileVersion("1.0.2")]
+[assembly: AssemblyInformationalVersion("1.0.2")]
