662ac6cc68330a5a2fac362941fe527ca56993bd
Evan Moore <evan.moore@cityofnorthbay.ca>
2020-09-15 15:13:13 -0400

    Add test cases

--- src/UmbracoExamine.PDF.Tests/PdfTextServiceTest.cs
+++ src/UmbracoExamine.PDF.Tests/PdfTextServiceTest.cs
@@ -37,6 +37,8 @@ namespace UmbracoExamine.PDF.Tests
         [TestCase("SurviorFlipCup.pdf", "Drink the beer", "game", "following")]
         [TestCase("Office16Word.pdf", "this", "is", "simple", "word")]
         [TestCase("windows-vista.pdf", "WINDOWS", "VISTA", "ULTIMATE", "supplements", "Anytime Upgrade", "it is invalid")]
+        [TestCase("file-example_pdf_1mb.pdf", "Lorem ipsum", "Vestibulum", "vehicula")]
+        [TestCase("11.pdf", "How to use the tools")]
         public void ExtractText__must_return_the_text_from_the_file_on_the_path(string pdfFileName,
             params string[] expectedSentences)
         {
@@ -48,19 +50,24 @@ namespace UmbracoExamine.PDF.Tests
                 StringAssert.Contains(expectedSentence.ToLower(), text);
             }
         }
-        
-        //[Test]
-        //[TestCase("windows-vista.pdf", "WINDOWS", "VISTA", "ULTIMATE", "supplements", "Anytime Upgrade","it is invalid")]
-        //public void ExtractText__it_is_a_known_issue_we_cant_handle_some_files(string pdfFileName,
-        //    params string[] expectedSentences)
-        //{
-        //    var absolutePath = Path.GetFullPath(Path.Combine(_testFilesDir.FullName, "TestFiles/", pdfFileName));
-        //    var text = _sut.ExtractText(absolutePath).ToLower();
 
-        //    foreach (var expectedSentence in expectedSentences)
-        //    {
-        //        StringAssert.DoesNotContain(expectedSentence.ToLower(), text, "If this test fails, it is actually a success. Then we can suddenly handle a pdf file, that was known not to be handled.");
-        //    }
-        //}
+        /// <summary>
+        /// Adobe Reader can't read text out of this, so I don't think it's a crime that we can't either.
+        /// </summary>
+        /// <param name="pdfFileName"></param>
+        /// <param name="expectedSentences"></param>
+        [Test]
+        [TestCase("12.pdf", "This here be some text")]
+        public void ExtractText__it_is_a_known_issue_we_cant_handle_some_files(string pdfFileName,
+            params string[] expectedSentences)
+        {
+            var absolutePath = Path.GetFullPath(Path.Combine(_testFilesDir.FullName, "TestFiles/", pdfFileName));
+            var text = _pdfTextService.ExtractText(absolutePath).ToLower();
+
+            foreach (var expectedSentence in expectedSentences)
+            {
+                StringAssert.DoesNotContain(expectedSentence.ToLower(), text, "If this test fails, it is actually a success. Then we can suddenly handle a pdf file, that was known not to be handled.");
+            }
+        }
     }
 }
